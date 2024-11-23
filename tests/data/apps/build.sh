33cb64feda04351b3eec13d12d4891c6adb8317e
reubenmiller <reuben.d.miller@gmail.com>
2024-11-23 21:27:54 +0100

    add tests

--- tests/data/apps/build.sh
+++ tests/data/apps/build.sh
@@ -11,7 +11,9 @@ pushd "$SCRIPT_DIR" >/dev/null ||:
 (cd app2 && zip ../app2.zip docker-compose.yaml Dockerfile static/*)
 
 if command -V docker >/dev/null 2>&1; then
-    (cd app3 && docker build -t app3 . && docker save app3 > ../app3.tar)
+    (cd app3 && docker build --load -t app3 . && docker save app3 > ../app3.tar)
 fi
+
+(cd app4 && tar czvf ../app4.tar.gz docker-compose.yaml Dockerfile)
 
 popd ||:
