3f3156d754916b2cbb72b4820174c25ab085dca7
reubenmiller <reuben.d.miller@gmail.com>
2024-11-29 09:29:21 +0100

    feat(container-group): prune images during container-group finalize hook

--- cli/container_group/finalize.go
+++ cli/container_group/finalize.go
@@ -4,10 +4,14 @@ Copyright © 2024 thin-edge.io <info@thin-edge.io>
 package container_group
 
 import (
+	"context"
 	"log/slog"
 
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/go-units"
 	"github.com/spf13/cobra"
 	"github.com/thin-edge/tedge-container-plugin/pkg/cli"
+	"github.com/thin-edge/tedge-container-plugin/pkg/container"
 )
 
 func NewFinalizeCommand(ctx cli.Cli) *cobra.Command {
@@ -16,6 +20,25 @@ func NewFinalizeCommand(ctx cli.Cli) *cobra.Command {
 		Short: "Finalize container install/remove operation",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			slog.Info("Executing", "cmd", cmd.CalledAs(), "args", args)
+
+			pruneImages := ctx.GetBool("container.pruneImages")
+			if !pruneImages {
+				return nil
+			}
+			cli, err := container.NewContainerClient()
+			if err != nil {
+				return err
+			}
+			slog.Info("Pruning images")
+			ctx := context.Background()
+			resp, err := cli.Client.ImagesPrune(ctx, filters.Args{})
+			if err != nil {
+				return err
+			}
+			for _, image := range resp.ImagesDeleted {
+				slog.Info("Deleted image.", "deleted", image.Deleted, "untagged", image.Untagged)
+			}
+			slog.Info("Reclaimed space.", "size", units.HumanSizeWithPrecision(float64(resp.SpaceReclaimed), 3))
 			return nil
 		},
 	}
