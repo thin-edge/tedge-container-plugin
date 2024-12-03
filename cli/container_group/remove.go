6f0dca8711f8bbe63f01e3549fed4fd7b92a9898
reubenmiller <reuben.d.miller@gmail.com>
2024-12-03 22:13:19 +0100

    feat(container-group): improve logging when removing a compose project

--- cli/container_group/remove.go
+++ cli/container_group/remove.go
@@ -6,6 +6,7 @@ package container_group
 import (
 	"context"
 	"log/slog"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/thin-edge/tedge-container-plugin/pkg/cli"
@@ -15,29 +16,39 @@ import (
 type RemoveCommand struct {
 	*cobra.Command
 
-	ModuleVersion string
+	CommandContext cli.Cli
+	ModuleVersion  string
 }
 
 // removeCmd represents the remove command
 func NewRemoveCommand(ctx cli.Cli) *cobra.Command {
-	command := &RemoveCommand{}
+	command := &RemoveCommand{
+		CommandContext: ctx,
+	}
 	cmd := &cobra.Command{
 		Use:   "remove",
 		Short: "Remove a container",
 		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			slog.Info("Executing", "cmd", cmd.CalledAs(), "args", args)
-			ctx := context.Background()
-			projectName := args[0]
-
-			cli, err := container.NewContainerClient()
-			if err != nil {
-				return err
-			}
-
-			return cli.ComposeDown(ctx, cmd.ErrOrStderr(), projectName)
-		},
+		RunE:  command.RunE,
 	}
 	cmd.Flags().StringVar(&command.ModuleVersion, "module-version", "", "Software version to remove")
 	return cmd
+}
+
+func (c *RemoveCommand) RunE(cmd *cobra.Command, args []string) error {
+	slog.Info("Executing", "cmd", cmd.CalledAs(), "args", args)
+	ctx := context.Background()
+	projectName := args[0]
+
+	cli, err := container.NewContainerClient()
+	if err != nil {
+		return err
+	}
+
+	persistentDir, err := c.CommandContext.PersistentDir(false)
+	if err != nil {
+		return err
+	}
+	workingDir := filepath.Join(persistentDir, "compose", projectName)
+	return cli.ComposeDown(ctx, cmd.ErrOrStderr(), projectName, workingDir)
 }
